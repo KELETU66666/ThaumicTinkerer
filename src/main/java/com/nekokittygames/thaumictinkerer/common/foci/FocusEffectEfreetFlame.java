d4b7ede136524281901dec643834c6ec450fda9c
Me <135455255+IcarussOne@users.noreply.github.com>
2024-08-26 02:17:16 -0500

    Add particles and sounds to Efreet's Flame

--- src/main/java/com/nekokittygames/thaumictinkerer/common/foci/FocusEffectEfreetFlame.java
+++ src/main/java/com/nekokittygames/thaumictinkerer/common/foci/FocusEffectEfreetFlame.java
@@ -2,6 +2,7 @@ package com.nekokittygames.thaumictinkerer.common.foci;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
@@ -10,10 +11,17 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.casters.FocusEffect;
 import thaumcraft.api.casters.ICaster;
 import thaumcraft.api.casters.Trajectory;
+import thaumcraft.client.fx.ParticleEngine;
+import thaumcraft.client.fx.particles.FXGeneric;
+import thaumcraft.common.lib.network.PacketHandler;
+import thaumcraft.common.lib.network.fx.PacketFXFocusPartImpact;
 
 @SuppressWarnings("deprecation")
 public class FocusEffectEfreetFlame extends FocusEffect {
@@ -21,6 +29,9 @@ public class FocusEffectEfreetFlame extends FocusEffect {
 
     @Override
     public boolean execute(RayTraceResult target, Trajectory trajectory, float finalPower, int num) {
+        PacketHandler.INSTANCE.sendToAllAround(new PacketFXFocusPartImpact(target.hitVec.x, target.hitVec.y, target.hitVec.z, new String[]{this.getKey()}), new NetworkRegistry.TargetPoint(this.getPackage().world.provider.getDimension(), target.hitVec.x, target.hitVec.y, target.hitVec.z, 64.0D));
+        this.getPackage().world.playSound(null, target.hitVec.x, target.hitVec.y, target.hitVec.z, SoundEvents.BLOCK_FIRE_AMBIENT, SoundCategory.PLAYERS, 1.0F, 1.5F + (float) (this.getPackage().world.rand.nextGaussian() * 0.05F));
+
         if (target.typeOfHit == RayTraceResult.Type.BLOCK) {
             ItemStack casterStack = ItemStack.EMPTY;
             getPackage().getCaster().getHeldItemMainhand();
@@ -61,8 +72,23 @@ public class FocusEffectEfreetFlame extends FocusEffect {
     }
 
     @Override
-    public void renderParticleFX(World world, double v, double v1, double v2, double v3, double v4, double v5) {
-        // Empty
+    public void onCast(Entity caster) {
+        caster.world.playSound(null, caster.getPosition().up(), SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.PLAYERS, 1.0F, 0.5F + caster.world.rand.nextFloat() * 0.05F);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void renderParticleFX(World world, double posX, double posY, double posZ, double velX, double velY, double velZ) {
+        final FXGeneric pp = new FXGeneric(world, posX, posY, posZ, velX, velY, velZ);
+        int color = 25565230;
+        pp.setAlphaF(0.7F);
+        pp.setGravity(-0.2F);
+        pp.setMaxAge(10);
+        pp.setParticles(640, 10, 1);
+        pp.setRBGColorF(((color >> 16) & 0xFF) / 255.0F, ((color >> 8) & 0xFF) / 255.0F, (color & 0xFF) / 255.0F);
+        pp.setSlowDown(0.75D);
+        pp.setScale((float) (1.5F + world.rand.nextGaussian() * 0.2F));
+        ParticleEngine.addEffect(world, pp);
     }
 
     @Override
