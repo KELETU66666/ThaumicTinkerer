b342f6e39c9dd3de77c815d559827bc2246ee00c
Me <135455255+IcarussOne@users.noreply.github.com>
2024-08-26 04:00:22 -0500

    Add particles and sounds to Telekinesis

--- src/main/java/com/nekokittygames/thaumictinkerer/common/foci/FocusEffectTelekenesis.java
+++ src/main/java/com/nekokittygames/thaumictinkerer/common/foci/FocusEffectTelekenesis.java
@@ -2,14 +2,23 @@ package com.nekokittygames.thaumictinkerer.common.foci;
 
 import com.nekokittygames.thaumictinkerer.common.misc.MiscHelper;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.casters.FocusEffect;
 import thaumcraft.api.casters.NodeSetting;
 import thaumcraft.api.casters.Trajectory;
+import thaumcraft.client.fx.ParticleEngine;
+import thaumcraft.client.fx.particles.FXGeneric;
 import thaumcraft.codechicken.lib.vec.Vector3;
+import thaumcraft.common.lib.SoundsTC;
+import thaumcraft.common.lib.network.PacketHandler;
+import thaumcraft.common.lib.network.fx.PacketFXFocusPartImpact;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -17,6 +26,9 @@ import java.util.List;
 public class FocusEffectTelekenesis extends FocusEffect {
     @Override
     public boolean execute(RayTraceResult paramRayTraceResult, @Nullable Trajectory paramTrajectory, float paramFloat, int paramInt) {
+        PacketHandler.INSTANCE.sendToAllAround(new PacketFXFocusPartImpact(target.hitVec.x, target.hitVec.y, target.hitVec.z, new String[]{this.getKey()}), new NetworkRegistry.TargetPoint(this.getPackage().world.provider.getDimension(), target.hitVec.x, target.hitVec.y, target.hitVec.z, 64.0D));
+        this.getPackage().world.playSound(null, target.hitVec.x, target.hitVec.y, target.hitVec.z, SoundsTC.scan, SoundCategory.PLAYERS, 0.7F, 1.4F + (float) (this.getPackage().getCaster().world.rand.nextGaussian() * 0.05F));
+
         if (paramRayTraceResult.typeOfHit != RayTraceResult.Type.BLOCK)
             return false;
         if (paramTrajectory == null)
@@ -44,8 +56,23 @@ public class FocusEffectTelekenesis extends FocusEffect {
     }
 
     @Override
-    public void renderParticleFX(World paramWorld, double paramDouble1, double paramDouble2, double paramDouble3, double paramDouble4, double paramDouble5, double paramDouble6) {
-        // empty
+    public void onCast(Entity caster) {
+        caster.world.playSound(null, caster.getPosition().up(), SoundsTC.hhoff, SoundCategory.PLAYERS, 0.8F, 1.15F + (float) (caster.world.rand.nextGaussian() * 0.05F));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void renderParticleFX(World world, double posX, double posY, double posZ, double velX, double velY, double velZ) {
+        final FXGeneric pp = new FXGeneric(world, posX, posY, posZ, velX, velY, velZ);
+        pp.setMaxAge(9);
+        pp.setRBGColorF(0.25f + world.rand.nextFloat() * 0.25F, 0.25F + world.rand.nextFloat() * 0.25F, 0.25F + world.rand.nextFloat() * 0.25F);
+        pp.setAlphaF(new float[]{0.0F, 0.6F, 0.6F, 0.0F});
+        pp.setGridSize(64);
+        pp.setParticles(448, 9, 1);
+        pp.setScale(new float[]{0.5F, 0.25F});
+        pp.setGravity((float) (world.rand.nextGaussian() * 0.009F));
+        pp.setRandomMovementScale(0.0025F, 0.0025F, 0.0025F);
+        ParticleEngine.addEffect(world, (Particle) pp);
     }
 
     @Override
